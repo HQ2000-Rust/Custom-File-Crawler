f5d725e694ff6c9a3d61f9ddcd5429630a7e65aa
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-09-07 23:02:24 +0200

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,10 @@
-pub use crate::builder::{
-    Crawler,
-    marker::{Async, NonAsync},
-};
+#[cfg(any(feature = "async", feature = "lazy_store", doc))]
+pub use crate::builder::marker::Async;
+#[cfg(any(feature = "parallel", feature = "lazy_store", doc))]
+pub use crate::builder::marker::NonAsync;
+pub use crate::builder::{Crawler, context::NoContext};
+
+#[cfg(any(feature = "parallel", doc))]
+pub use rayon;
+#[cfg(any(feature = "async", doc))]
 pub use tokio;
