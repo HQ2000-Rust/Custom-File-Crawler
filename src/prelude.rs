8ed17010de9c1cd33a267deb256aea8fd3e9b946
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-09-08 20:28:56 +0200

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,5 @@
+//! The prelude. It is - as you see - very small, so if you include it, you don't have to worry about importing items as well as namespace pollution.
+
 #[cfg(any(feature = "async", feature = "lazy_store", doc))]
 pub use crate::builder::marker::Async;
 #[cfg(any(feature = "parallel", feature = "lazy_store", doc))]
